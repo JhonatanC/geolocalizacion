f6966ac68767d2cb83067e5b69892a84c6fd739f
JhonatanC <jhonatanchurtado@outlook.com>
2016-05-11 11:19:08 -0500

    Probando google api v3

--- js/location.js
+++ js/location.js
@@ -3,7 +3,11 @@ $(function(){
 
 	var mapa = $('#mapa');
 
-	function success(result){
+	/*
+	 * Para mapa estatico
+	 */
+
+	/*function success(result){
 		var lat = result.coords.latitude;
 		var lon = result.coords.longitude;
 		var coor = lat + "," + lon;
@@ -13,5 +17,31 @@ $(function(){
 
 	function error(){
 		console.log('Hubo un error');
+	}*/
+
+
+	/*
+	 * Para mapa dinánmico
+	 */
+
+	function success(result){
+		var lat = result.coords.latitude;
+		var lon = result.coords.longitude;
+		var coor = lat + "," + lon;
+
+		//Objetos de googles. (instancias 'palabra new')
+		var gLatLon = new google.maps.LatLng(coor);
+		var objConfig = {
+			zoom : 17,
+			center : gLatLon
+		}
+
+		//recibe 2 parámetros, donde se va a renderizar el mapa, y una objeto de configuración.  
+		var gMapa = new google.maps.Map(mapa,objConfig);
 	}
+
+	function error(){
+		console.log('Hubo un error');
+	}
+
 });
